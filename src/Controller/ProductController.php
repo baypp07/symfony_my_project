c5569ded4e2da874a876914eb3f8e119f4d74328
baypp07 <pphouone@gmail.com>
2025-12-04 00:53:01 +0100

    database config

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
+
+final class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product')]
+   public function index(ProductRepository $repository): Response
+    {
+    
+
+        return $this->render('product/index.html.twig', [
+            'products' => $repository->findAll(),
+        ]);
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Product;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $product = new Product;
+        $product ->setName('Product one');
+        $product ->setDescription("This is the first product");
+        $product ->setSize(100);
+        
+        $manager->persist($product);
+
+        $product = new Product;
+        $product ->setName('Product two');
+        $product ->setDescription("this is the second product");
+        $product ->setSize(200);
+        $manager->persist($product);
+
+        $manager->flush();
+    }
+}
