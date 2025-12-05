4f1e275bec826aabf8a52f94cbc71ad8888daf8c
baypp07 <pphouone@gmail.com>
2025-12-05 23:57:30 +0100

    show product id controller product
    show product & new product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
+use App\Entity\Product;
 
 final class ProductController extends AbstractController
 {
@@ -18,9 +19,21 @@ final class ProductController extends AbstractController
             'products' => $repository->findAll(),
         ]);
     }       
- #[Route('/product/{id}')]
-        public function show($id) : Response
+    #[Route('/product/{id<\d+>}', name:'product_show')]
+        public function show(Product $product) : Response
         {
-        dd($id);
+         return $this->render('product/show.html.twig',[
+            'product' => $product
+         ]);
             }
+
+    #[Route('/product/new', name:'product_new')]
+        public function new(): Response{
+            return $this->render('product/new.html.twig');
         }
+
+
+        }
+
+        
+  
